1327474ec5d558748fd3971b0bf37459be9a528d
Sergey Novikov <novikov.su@gmail.com>
2013-03-06 00:07:05 +0600

    Quick start with jPlayer

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 PATH="$PATH:/home/novikov/apps/s3cmd-1.1.0-beta3"
 
-s3cmd put --acl-public index.html s3://playchat.radio-t.com/index.html
-s3cmd put --acl-public assets/audiojs/audio.js s3://playchat.radio-t.com/assets/audiojs/audio.js
-s3cmd put --acl-public assets/audiojs/audiojs.swf s3://playchat.radio-t.com/assets/audiojs/audiojs.swf
-s3cmd put --acl-public assets/audiojs/player-graphics.gif s3://playchat.radio-t.com/assets/audiojs/player-graphics.gif
-s3cmd put --acl-public assets/images/favicon.png s3://playchat.radio-t.com/assets/images/favicon.png
+s3cmd put --acl-public index.html s3://dev.playchat.radio-t.com/index.html
+s3cmd put --acl-public assets/audiojs/audio.js s3://dev.playchat.radio-t.com/assets/audiojs/audio.js
+s3cmd put --acl-public assets/audiojs/audiojs.swf s3://dev.playchat.radio-t.com/assets/audiojs/audiojs.swf
+s3cmd put --acl-public assets/audiojs/player-graphics.gif s3://dev.playchat.radio-t.com/assets/audiojs/player-graphics.gif
+s3cmd put --acl-public assets/images/favicon.png s3://dev.playchat.radio-t.com/assets/images/favicon.png
