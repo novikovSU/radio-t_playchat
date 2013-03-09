6e7636dbda837a8cca5df6e19686795bd88b7838
Sergey Novikov <novikov.su@gmail.com>
2013-03-09 23:37:28 +0600

    jPlayer integrated with default skin.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 PATH="$PATH:/home/novikov/apps/s3cmd-1.1.0-beta3"
 
 s3cmd put --acl-public index.html s3://dev.playchat.radio-t.com/index.html
-s3cmd put --acl-public assets/audiojs/audio.js s3://dev.playchat.radio-t.com/assets/audiojs/audio.js
-s3cmd put --acl-public assets/audiojs/audiojs.swf s3://dev.playchat.radio-t.com/assets/audiojs/audiojs.swf
-s3cmd put --acl-public assets/audiojs/player-graphics.gif s3://dev.playchat.radio-t.com/assets/audiojs/player-graphics.gif
-s3cmd put --acl-public assets/images/favicon.png s3://dev.playchat.radio-t.com/assets/images/favicon.png
+#s3cmd put --acl-public assets/images/favicon.png s3://dev.playchat.radio-t.com/assets/images/favicon.png
+#s3cmd put --acl-public assets/jplayer/jquery.jplayer.min.js s3://dev.playchat.radio-t.com/assets/jplayer/jquery.jplayer.min.js
+#s3cmd put --acl-public assets/jplayer/Jplayer.swf s3://dev.playchat.radio-t.com/assets/jplayer/Jplayer.swf
+#s3cmd put --acl-public --recursive assets/jplayer/skins s3://dev.playchat.radio-t.com/assets/jplayer/
