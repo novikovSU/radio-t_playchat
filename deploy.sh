e7f9ffa0194e20c4abab5ebba05927a5eace282f
Sergey Novikov <novikov.su@gmail.com>
2013-03-09 23:45:53 +0600

    fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 
 PATH="$PATH:/home/novikov/apps/s3cmd-1.1.0-beta3"
 
-s3cmd put --acl-public index.html s3://dev.playchat.radio-t.com/index.html
-#s3cmd put --acl-public assets/images/favicon.png s3://dev.playchat.radio-t.com/assets/images/favicon.png
-#s3cmd put --acl-public assets/jplayer/jquery.jplayer.min.js s3://dev.playchat.radio-t.com/assets/jplayer/jquery.jplayer.min.js
-#s3cmd put --acl-public assets/jplayer/Jplayer.swf s3://dev.playchat.radio-t.com/assets/jplayer/Jplayer.swf
-#s3cmd put --acl-public --recursive assets/jplayer/skins s3://dev.playchat.radio-t.com/assets/jplayer/
+s3cmd put --acl-public index.html s3://playchat.radio-t.com/index.html
+s3cmd put --acl-public assets/images/favicon.png s3://playchat.radio-t.com/assets/images/favicon.png
+s3cmd put --acl-public --recursive assets/jplayer s3://playchat.radio-t.com/assets/
