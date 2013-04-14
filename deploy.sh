3c8f6a878736df8a948e474f6a208bde50c66add
Sergey Novikov <novikov.su@gmail.com>
2013-04-14 17:10:15 +0600

    Prepare for moving from Firebase to MongoHQ.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-PATH="$PATH:/home/novikov/apps/s3cmd-1.1.0-beta3"
+PATH="$PATH:/home/novikov/apps/s3cmd-1.5.0"
 
 s3cmd put --acl-public index.html s3://playchat.radio-t.com/index.html
-s3cmd put --acl-public assets/images/favicon.png s3://playchat.radio-t.com/assets/images/favicon.png
-s3cmd put --acl-public --recursive assets/jplayer s3://playchat.radio-t.com/assets/
+#s3cmd put --acl-public assets/images/favicon.png s3://playchat.radio-t.com/assets/images/favicon.png
+#s3cmd put --acl-public --recursive assets/jplayer s3://playchat.radio-t.com/assets/
