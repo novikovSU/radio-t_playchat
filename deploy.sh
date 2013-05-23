6f071767ae356b587e9a05381e27e3dde2b22616
Sergey Novikov <novikov.su@gmail.com>
2013-05-23 13:21:15 +0600

    after stash pop

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ PATH="$PATH:/home/novikov/apps/s3cmd-1.5.0"
 
 s3cmd put --acl-public index.html s3://playchat.radio-t.com/index.html
 #s3cmd put --acl-public assets/images/favicon.png s3://playchat.radio-t.com/assets/images/favicon.png
-#s3cmd put --acl-public --recursive assets/jplayer s3://playchat.radio-t.com/assets/
+#s3cmd put --acl-public --recursive assets s3://playchat.radio-t.com/
+#s3cmd put --acl-public assets/candy/default.css s3://playchat.radio-t.com/assets/candy/
