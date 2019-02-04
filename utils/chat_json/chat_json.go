019b25f717009d5a8b9283f582f55b6f152d33de
Sergey Novikov <novikov@Sergeys-Air.metamodel.ru>
2019-02-04 13:15:59 +0500

    Add issue 635

--- utils/chat_json/chat_json.go
+++ utils/chat_json/chat_json.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	issue    = 634
+	issue    = 635
 	issueStr = fmt.Sprintf("%d", issue)
 	chatURL  = "https://chat.radio-t.com/logs/radio-t-" + issueStr + ".html"
 	chatFile = "../../data/" + issueStr + "/radio-t-" + issueStr + ".html"
--- utils/srt_json/srt_json.go
+++ utils/srt_json/srt_json.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	issue    = 634
+	issue    = 635
 	issueStr = fmt.Sprintf("%d", issue)
 	srtFile  = "../../data/" + issueStr + "/src/rt_podcast" + issueStr + ".srt"
 	jsonFile = "../../data/" + issueStr + "/src/rt_podcast" + issueStr + ".json"
